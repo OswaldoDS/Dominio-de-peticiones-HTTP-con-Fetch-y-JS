eccbefc4314116843ffbba31ecfb60ad4dae15b6
Oswaldo <santoswaldos@gmail.com>
2026-04-01 20:59:44 -0600

    Aplicando try catch a la función crearPost

--- script.js
+++ script.js
@@ -23,26 +23,31 @@ async function leerPosts() {
 //Tarea 2
 //Post ---> Crear recursos
 async function crearPost() {
-  //Creando el nuevo recurso
-  const nuevoPost = {
-    title: "Proyecto de ingeniería",
-    body: "Implementación de arquitectua RESTful",
-    userId: 1,
-  };
+  try {
+    //Creando el nuevo recurso
+    const nuevoPost = {
+      title: "Proyecto de ingeniería",
+      body: "Implementación de arquitectua RESTful",
+      userId: 1,
+    };
 
-  //Creando la respuesta
-  const respuesta = await fetch(BASE_URL, {
-    method: "POST", //Indica el tipo de petición puede ser GET/POST/PUT/PATCH/DELETE
-    body: JSON.stringify(nuevoPost), //Para mandar los datos al servidor
-    headers: { "Content-type": "application/json; charset=UTF-8" }, // Le dice al servidor que tipo de datos se están enviando
-  });
+    //Creando la respuesta
+    const respuesta = await fetch(BASE_URL, {
+      method: "POST", //Indica el tipo de petición puede ser GET/POST/PUT/PATCH/DELETE
+      body: JSON.stringify(nuevoPost), //Para mandar los datos al servidor
+      headers: { "Content-type": "application/json; charset=UTF-8" }, // Le dice al servidor que tipo de datos se están enviando
+    });
 
-  const datos = await respuesta.json();
-  console.log("Tarea 2 - POST: Recurso creado: ", datos);
-  // console.log(Object.keys(datos)); //Para visualizar las keys que genera
-  // console.log(Object.values(datos)); //Para visualizar los valores de las keys
+    const datos = await respuesta.json();
+    console.log("Tarea 2 - POST: Recurso creado: ", datos);
+    // console.log(Object.keys(datos)); //Para visualizar las keys que genera
+    // console.log(Object.values(datos)); //Para visualizar los valores de las keys
 
-  //El orden de las propiedades son title -> body -> userId -> id (lo asigna el servidor siendo el Id único)
+    // El orden de las propiedades son:
+    // title -> body -> userId -> id (lo asigna el servidor siendo el Id único)
+  } catch (error) {
+    console.error("Fallo en GET: ", error.message);
+  }
 }
 
 //Función de ejecución del flujo
