f106b6a00641011f42643b35111307ea5d13d48f
Oswaldo <santoswaldos@gmail.com>
2026-04-01 19:32:29 -0600

    Uso de GET para leer datos

--- script.js
+++ script.js
@@ -2,4 +2,25 @@
 require("dotenv").config();
 
 //Referenciamos la variable de entorno .env
-const BASE_URL = process.env.URL_API;
+const BASE_URL = process.env.API_URL;
+
+//Tarea 1
+//GET ---> Leer datos
+async function leerPosts() {
+  try {
+    const respuesta = await fetch(BASE_URL);
+    if (!respuesta.ok) throw new Error("Error al obtener datos");
+
+    const datos = await respuesta.json();
+    console.log("Tarea 1 - GET: Primeros 2 registros", datos);
+  } catch (error) {
+    console.error("Fallo en GET: ", error.message);
+  }
+}
+
+async function ejecutarPractica() {
+  console.log("Ejecutando...");
+  await leerPosts();
+}
+
+ejecutarPractica();
